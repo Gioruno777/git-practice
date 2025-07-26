e5330f9662ce7b67dd9771d6843f73ad9cf741ee
Gioruno777 <103649653+Gioruno777@users.noreply.github.com>
2025-07-26 18:59:44 +0800

    B changes app.js

--- Hello1.java
+++ Hello1.java
@@ -2,5 +2,6 @@ public class Hello1 {
     public static void main(String[] args) {
         System.out.println("Hello1, world!");
         System.out.println("B file line 2 - from A!");
+        // UserB
     }
 }
