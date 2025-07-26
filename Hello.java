8a9706e82202b280eed771b3282713cc6f7e7480
Gioruno777 <103649653+Gioruno777@users.noreply.github.com>
2025-07-26 18:44:55 +0800

    Feature branch updates b

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("A file line 2 - from B");
         System.out.println("A file line 2 - from A");
+        // A file line 2 - merged A and B
     }
 }
